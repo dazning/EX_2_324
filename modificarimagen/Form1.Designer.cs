b3fc4eb9f3e2339e34b8c151e37c1a542bf298c8
dazning <38385664+dazning@users.noreply.github.com>
2023-12-11 04:03:50 -0400

    Add files via upload

---
+++ modificarimagen/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace imagenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(300, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(362, 352);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(69, 132);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(70, 160);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button5.Location = new System.Drawing.Point(432, 396);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 28);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Modificar";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(69, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Subir Imagen";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(166, 306);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Tercer Color";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(166, 277);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Segundo Color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(166, 248);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Primer Color";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::imagenes.Properties.Resources.maxresdefault;
+            this.ClientSize = new System.Drawing.Size(917, 540);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ modificarimagen/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace imagenes
+{
+    public partial class Form1 : Form
+    {
+        int Rm, Gm, Bm;
+        int Rmc, Gmc, Bmc, L = 10;
+        private Color color1 = Color.Empty;
+        private Color color2 = Color.Empty;
+        private Color color3 = Color.Empty;
+
+        string connectionString = "Server=localhost;Database=rgb;Uid=root;Pwd=;";
+        MySqlConnection con;
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            // Inicializar la conexión a MySQL
+            con = new MySqlConnection(connectionString);
+
+            // Abre la conexión a MySQL
+            try
+            {
+                con.Open();
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al establecer la conexión a MySQL: {ex.Message}", "Error de Conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Cierra la conexión a MySQL
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+
+      
+
+       
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = string.Empty;
+            openFileDialog1.Filter = "Archivo JPG|*.JPG|Archivos BMP|*.bmp";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != string.Empty)
+            {
+                Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void AplicarCambiosEnImagen(Dictionary<Color, Color> mapeoColores, int umbralDistancia)
+        {
+            try
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                Bitmap bmpModificado = new Bitmap(bmp.Width, bmp.Height);
+
+                // Iterar sobre cada píxel en la imagen
+                for (int i = 0; i < bmp.Width; i++)
+                {
+                    for (int j = 0; j < bmp.Height; j++)
+                    {
+                        Color colorOriginal = bmp.GetPixel(i, j);
+
+                        // Buscar el color más cercano en el diccionario
+                        Color colorCercano = EncontrarColorCercano(colorOriginal, mapeoColores, umbralDistancia);
+
+                        // Asignar el color encontrado o mantener el original si no se encontró ninguno cercano
+                        bmpModificado.SetPixel(i, j, colorCercano);
+                    }
+                }
+
+                // Mostrar la imagen modificada en el PictureBox
+                pictureBox1.Image = bmpModificado;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al aplicar cambios en la imagen: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Color EncontrarColorCercano(Color color, Dictionary<Color, Color> mapeoColores, int umbralDistancia)
+        {
+            Color colorCercano = color;
+
+            foreach (var colorOriginal in mapeoColores.Keys)
+            {
+                int distancia = CalcularDistanciaColor(color, colorOriginal);
+
+                if (distancia < umbralDistancia)
+                {
+                    umbralDistancia = distancia;
+                    colorCercano = mapeoColores[colorOriginal];
+                }
+            }
+
+            return colorCercano;
+        }
+
+        private int CalcularDistanciaColor(Color c1, Color c2)
+        {
+            int distanciaR = Math.Abs(c1.R - c2.R);
+            int distanciaG = Math.Abs(c1.G - c2.G);
+            int distanciaB = Math.Abs(c1.B - c2.B);
+
+            return distanciaR + distanciaG + distanciaB;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (color1 != Color.Empty && color2 != Color.Empty && color3 != Color.Empty)
+            {
+                // Obtener los últimos tres colores de la base de datos
+                List<Color> coloresOriginales = ObtenerUltimosTresColores();
+
+                // Crear un diccionario para mapear los colores originales a los nuevos
+                Dictionary<Color, Color> mapeoColores = new Dictionary<Color, Color>
+        {
+            { coloresOriginales[0], color1 },
+            { coloresOriginales[1], color2 },
+            { coloresOriginales[2], color3 }
+        };
+
+                // Definir el umbral de distancia para buscar colores cercanos
+                int umbralDistancia = 50; // ajusta según tus necesidades
+
+                // Aplicar los cambios en la imagen
+                AplicarCambiosEnImagen(mapeoColores, umbralDistancia);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona los tres colores antes de modificar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                color1 = colorDialog1.Color;
+                button2.BackColor = color1;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                color2 = colorDialog1.Color;
+                button3.BackColor = color2;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                color3 = colorDialog1.Color;
+                button4.BackColor = color3;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<Color> ObtenerUltimosTresColores()
+        {
+            List<Color> colores = new List<Color>();
+
+            try
+            {
+                // Abrir la conexión a MySQL
+                con.Open();
+
+                // Consulta SQL para obtener los últimos tres colores de la base de datos
+                string query = "SELECT R, G, B FROM colores ORDER BY Id DESC LIMIT 3";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, con))
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int r = Convert.ToInt32(reader["R"]);
+                        int g = Convert.ToInt32(reader["G"]);
+                        int b = Convert.ToInt32(reader["B"]);
+                        colores.Add(Color.FromArgb(r, g, b));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar el error si es necesario
+                MessageBox.Show($"Error al obtener los últimos tres colores de la base de datos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Cerrar la conexión a MySQL
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+
+            return colores;
+        }
+
+
+      
+
+        private void GuardarColorEnBaseDeDatos(Color color)
+        {
+            try
+            {
+                // Abrir la conexión a MySQL
+                con.Open();
+
+                // Consulta SQL para insertar el color en la base de datos
+                string query = "INSERT INTO colores (R, G, B) VALUES (@R, @G, @B)";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, con))
+                {
+                    // Parámetros de la consulta
+                    cmd.Parameters.AddWithValue("@R", color.R);
+                    cmd.Parameters.AddWithValue("@G", color.G);
+                    cmd.Parameters.AddWithValue("@B", color.B);
+
+                    // Ejecutar la consulta
+                    cmd.ExecuteNonQuery();
+
+                    // Mostrar un mensaje de éxito
+                    MessageBox.Show("Color guardado en la base de datos correctamente", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Mostrar un mensaje de error si ocurre un problema
+                MessageBox.Show($"Error al guardar el color en la base de datos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // Cerrar la conexión a MySQL
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+        private int coloresCapturadosCount = 0;
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Obtener el color del píxel en las coordenadas del clic
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            Color c = bmp.GetPixel(e.X, e.Y);
+
+            // Guardar el color en la base de datos
+            GuardarColorEnBaseDeDatos(c);         
+            Rm = c.R;
+            Gm = c.G;
+            Bm = c.B;
+            Rmc = 0; Gmc = 0; Bmc = 0;
+            textBox1.Text = c.R.ToString();
+            textBox2.Text = c.G.ToString();
+            textBox3.Text = c.B.ToString();
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+
+    }
+}
